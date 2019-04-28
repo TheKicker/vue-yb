98234ac9922b1c99a2064edcdd94ea1e6f7968df
Cav Lemasters <cav.lemasters@geneva.edu>
2019-04-28 15:09:40 -0400

    April 28 Update: Presentation

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,20 +4,27 @@
         <h1 class="Htext">We Are Yellow Bucket</h1>
         <hr>
           <p> <strong>Welcome to our Break Early Test Application -- this is your window into the future of cinema.</strong></p>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
     </div>
     <div>
         <h3 class="Htext"> Cinema is for everyone </h3>
         <hr>
-          <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? </p>
-          <p> Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec venenatis tellus laoreet nisl interdum, ut tristique libero fringilla. Maecenas vel blandit eros. Nam elementum libero scelerisque gravida congue. Nullam at felis viverra, interdum erat et, ultricies metus. Aliquam sed lacinia lacus, ac tempor est. Ut dapibus quam a quam euismod, egestas facilisis nisl semper. In hac habitasse platea dictumst. Ut luctus nibh quis turpis euismod rhoncus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In tristique libero eleifend urna tempus, id rutrum est pretium. Phasellus porta bibendum consectetur.</p>
+        <p> Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.</p>
+        <p> Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.  </p>
     </div>
     <div>
         <h3 class="Htext"> Everyone is for cinema </h3>
         <hr>
-          <p> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
-          <p> Donec tempor est augue, semper efficitur ipsum condimentum a. Quisque aliquet sapien et dolor convallis lacinia. Integer nec elementum lectus. Vestibulum nibh arcu, placerat id euismod id, consectetur non leo. Etiam sed massa ac eros volutpat consectetur ac ac erat. Sed mollis mollis dictum. Integer dapibus nisi massa, iaculis venenatis nibh feugiat vitae. Donec pulvinar mollis ligula, ut varius dolor ultrices in. Nunc fermentum erat eu diam euismod, ut pellentesque ligula blandit. Fusce tincidunt dictum varius. Vestibulum faucibus condimentum orci, a tincidunt nisi. Nunc sodales tellus justo, ut rhoncus est venenatis et. </p>
+        <p> Gouda fromage frais cheesy feet. When the cheese comes out everybody's happy everyone loves cut the cheese bocconcini cheddar cheese strings cheese slices stilton. Croque monsieur st. agur blue cheese cheddar cheesy grin chalk and cheese cheesecake port-salut fromage. Lancashire taleggio the big cheese bocconcini cheese strings feta goat bavarian bergkase. Cream cheese cut the cheese cheddar.</p>
+        <p> Cheese slices cheese and wine monterey jack. Cut the cheese say cheese cauliflower cheese st. agur blue cheese cheese on toast bavarian bergkase boursin roquefort. Bocconcini edam blue castello say cheese the big cheese stinking bishop squirty cheese melted cheese. Cheese on toast ricotta the big cheese cheeseburger monterey jack parmesan boursin bavarian bergkase. Stilton port-salut everyone loves.</p>
     </div>
       <div id="spacer"></div>
   </div>
 </template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    p{
+        text-align: left;
+    }
+</style>
